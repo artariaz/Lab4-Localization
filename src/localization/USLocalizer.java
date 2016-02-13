fefed8dff9992a6a4605d71df29f497a812cff0b
artariaz <riazrafat.arta@gmail.com>
2016-02-13 16:11:09 -0500

    Changes to USLocalization class

--- src/localization/USLocalizer.java
+++ src/localization/USLocalizer.java
@@ -8,9 +8,17 @@ public class USLocalizer extends Thread {
 	private boolean isFacingWall;
 	private SampleProvider usSensor;
 	private float[] usData;
-	
-	public enum LocalizationType { FALLING_EDGE, RISING_EDGE };
+
+	public enum LocalizationType {
+		FALLING_EDGE, RISING_EDGE
+	};
+
+	public enum Location {
+		LEFT, BOTTOM
+	};
+
 	private LocalizationType locType;
+	private Location location;
 
 	public USLocalizer(Navigator nav, SampleProvider usSensor, float[] usData, LocalizationType locType) {
 		this.nav = nav;
@@ -21,14 +29,31 @@ public class USLocalizer extends Thread {
 
 	public void run() {
 		nav.start();
+
+		// Set up a starting point
 		initialize();
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		// Look for what algorithm to follow (Rising Edge or Falling Edge)
+		switch (locType) {
+		case FALLING_EDGE:
+			switch (location) {
+			case LEFT:
+				break;
+			case BOTTOM:
+				break;
+			}
+			break;
+		case RISING_EDGE:
+			switch (location) {
+			case LEFT:
+				break;
+			case BOTTOM:
+				break;
+			}
+			break;
+
 		}
-		
+
 	}
 
 	public float getFilteredData() {
@@ -48,20 +73,32 @@ public class USLocalizer extends Thread {
 	private void initialize() {
 		// If it is facing wall
 		if (facingWall(getFilteredData())) {
+			this.location = Location.LEFT;
 			while (facingWall(getFilteredData())) {
 				nav.setState(Navigator.State.ROTATE);
 			}
 			// No longer facing a wall, stop the motors
 			nav.setState(Navigator.State.IDLE);
+			sleepThread(250);
 		}
 
 		// If it is not facing a wall
 		else {
+			this.location = Location.BOTTOM;
 			while (!(facingWall(getFilteredData()))) {
 				nav.setState(Navigator.State.ROTATE);
 			}
 			nav.setState(Navigator.State.IDLE);
+			sleepThread(250);
 		}
 	}
 
+	public void sleepThread(int amount) {
+		try {
+			Thread.sleep(amount);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
